631bb58d86923088c8120a5a37c22b088fbe8abe
Your Name <your-email@example.com>
2024-10-23 14:14:50 +0300

    make circle avatar and add text with font family

--- lib/view/card_view.dart
+++ lib/view/card_view.dart
@@ -5,8 +5,47 @@ class CardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xff274460),
+    return Scaffold(
+      backgroundColor: const Color(0xff274460),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              maxRadius: 95,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                maxRadius: 93,
+                backgroundImage: AssetImage(
+                  'images/1679170985199.jpg',
+                ),
+              ),
+            ),
+           const Text(
+              'Mazen Mamoun',
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.white,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white.withOpacity(0.4),
+                //fontFamily: 'Pacifico',
+              ),
+            ),
+            Divider(
+              thickness: 0.5,
+              indent: 35,
+              endIndent: 35,
+              color: Colors.white.withOpacity(0.4),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
